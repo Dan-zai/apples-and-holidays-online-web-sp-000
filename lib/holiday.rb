5a77ddd27b6bee07935561fdc31afde0caddbea2
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-11 21:48:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     puts holiday.to_s.capitalize + ":"
-    holiday.each do 
+    holiday.each do |
 
 end
 
