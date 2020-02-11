79566e1e32529312ec4f23f2758773041122d0cd
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-11 21:45:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,8 +46,9 @@ def all_winter_holiday_supplies(holiday_hash)
     item.each do |items|
       item_array << items
     end
-    
+item_array    
 end
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
